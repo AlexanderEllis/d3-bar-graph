0b350d3a127b6f310a5b099562511f4373f65aee
Alexander Ellis <ellisonjtk@gmail.com>
2017-04-04 15:24:52 -0400

    Figured out X axis ticks.  See d3-axis issue #35

--- bar-graph.js
+++ bar-graph.js
@@ -5,68 +5,67 @@
     let width =  +svg.attr('width');
     let height = +svg.attr('height');
     
-    let x = d3.scaleBand()
-        .rangeRound( [margin.left, width - margin.right] )
-        .paddingInner( .1 );
-
-    let y = d3.scaleLinear()
-        .range( [height - margin.bottom, margin.top] );
 
 
     let URL = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
     d3.json(URL,  function( error, data ) {
-        
     
-    x.domain( data.data.map( d => d[0] ) );
-    y.domain( [0, d3.max( data.data, d => +d[1] ) ] );
+        let x = d3.scaleBand()
+            .rangeRound( [margin.left, width - margin.right] )
+            .paddingInner( .1 );
+
+        let y = d3.scaleLinear()
+            .range( [height - margin.bottom, margin.top] );
+            
         
-    
-    
-    
-    let bar = svg.selectAll( 'g' )
-        .data(data.data)
-        .enter().append( 'g' )
-        .attr( 'transform', d => 'translate(' + x( d[0] ) + ',0)' )
+        x.domain( data.data.map( d => d[0] ) );
+        y.domain( [0, d3.max( data.data, d => +d[1] ) ] );
+            
         
         
+        
+        let bar = svg.selectAll( 'g' )
+            .data(data.data)
+            .enter().append( 'g' )
+            .attr( 'transform', d => 'translate(' + x( d[0] ) + ',0)' )
+            
+            
 
 
-    bar.append( 'rect' )
-        .attr( 'y', d => y( +d[1] ) )
-        .attr( 'height', d => height - y( +d[1] ) -  margin.bottom )
-        .attr( 'width', x.bandwidth() )
-        .on( 'mouseover', (d) => d3.select('.tooltip')
-                                    .style( 'visibility', 'visible' )
-                                    .html( d[0] + ': ' + d[1] ) )
-        .on("mousemove", () =>  d3.select('.tooltip')
-                                    .style("top", (d3.event.pageY - 50) + 'px')
-                                    .style("left", (d3.event.pageX + 5) + 'px'))
-        .on( 'mouseout', function() {
-            d3.select('.tooltip').style( 'visibility', 'hidden' )
-        })
-    
-    // HEY MAN
+        bar.append( 'rect' )
+            .attr( 'y', d => y( +d[1] ) )
+            .attr( 'height', d => height - y( +d[1] ) -  margin.bottom )
+            .attr( 'width', x.bandwidth() )
+            .on( 'mouseover', (d) => d3.select('.tooltip')
+                                        .style( 'visibility', 'visible' )
+                                        .html( d[0] + ': ' + d[1] ) )
+            .on("mousemove", () =>  d3.select('.tooltip')
+                                        .style("top", (d3.event.pageY - 50) + 'px')
+                                        .style("left", (d3.event.pageX + 5) + 'px'))
+            .on( 'mouseout', function() {
+                d3.select('.tooltip').style( 'visibility', 'hidden' )
+            })
+        
     d3.select('body').append( 'div' )
-        .attr( 'class', 'tooltip' )
-        .style( 'position', 'absolute' )
-        .style( 'z-index', '10' )
-        .style( 'visibility', 'hidden')
-        .style( 'height', '20px' )
-        .style( 'background-color', '#f4f4f4' )
-        .style( 'border', '1px solid #4286f4' )
-        .style( 'border-radius', '3px' )
-        .style( 'padding', '11px' )
+            .attr( 'class', 'tooltip' )
+            .style( 'position', 'absolute' )
+            .style( 'z-index', '10' )
+            .style( 'visibility', 'hidden')
+            .style( 'height', '20px' )
+            .style( 'background-color', '#f4f4f4' )
+            .style( 'border', '1px solid #4286f4' )
+            .style( 'border-radius', '3px' )
+            .style( 'padding', '11px' )
 
 
+        svg.append('g')
+            .attr('transform', 'translate(0,' + y(0) + ')')
+            .call(d3.axisBottom(x).tickValues(x.domain().filter((d, i) => !(i%20) )));
 
-    
-    svg.append('g')
-        .attr('transform', 'translate(0,' + y(0) + ')')
-        .call(d3.axisBottom(x).ticks(0, 10, 5));
 
-    svg.append('g')
-        .attr('transform', 'translate(' + margin.left + ',0)')
-        .call(d3.axisRight(y).ticks(6));
+        svg.append('g')
+            .attr('transform', 'translate(' + margin.left + ',0)')
+            .call(d3.axisLeft(y).ticks(6));
 
     });
